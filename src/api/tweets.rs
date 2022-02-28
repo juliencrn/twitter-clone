2afa4e5a6e223f75f48a0e41704b34a178f5e872
Julien <juliencaron@protonmail.com>
2022-02-28 03:47:41 +0100

    simplify route path param access

--- src/api/tweets.rs
+++ src/api/tweets.rs
@@ -3,8 +3,6 @@ use crate::db::DBPool;
 use crate::models::{Tweet, TweetRequest};
 use crate::response::Response;
 use actix_web::{delete, get, post, web, HttpResponse, Responder};
-
-use std::str::FromStr;
 use uuid::Uuid;
 
 type Pool = web::Data<DBPool>;
@@ -45,43 +43,36 @@ pub async fn create(tweet_req: web::Json<TweetRequest>, pool: Pool) -> impl Resp
 
 /// find a tweet by its id `/tweets/{id}`
 #[get("/tweets/{id}")]
-pub async fn find_one(path: web::Path<(String,)>, pool: web::Data<DBPool>) -> impl Responder {
-    if let Ok(uuid) = Uuid::from_str(path.0 .0.as_str()) {
-        // use web::block to offload blocking Diesel code without blocking server thread
-        let tweet = web::block(move || {
-            let conn = pool.get().expect(CONNECTION_POOL_ERROR);
-            Tweet::find(uuid, &conn)
-        })
-        .await;
+pub async fn find_one(uid: web::Path<Uuid>, pool: web::Data<DBPool>) -> impl Responder {
+    let tweet_id = uid.into_inner();
 
-        // match tweet {}
+    // use web::block to offload blocking Diesel code without blocking server thread
+    let tweet = web::block(move || {
+        let conn = pool.get().expect(CONNECTION_POOL_ERROR);
+        Tweet::find(tweet_id, &conn)
+    })
+    .await;
 
-        if let Ok(tweet) = tweet {
-            HttpResponse::Ok().json(tweet)
-        } else {
-            HttpResponse::NoContent().await.unwrap()
-        }
-    } else {
-        HttpResponse::BadRequest().await.unwrap()
+    match tweet {
+        Ok(tweet) => HttpResponse::Ok().json(tweet),
+        Err(_) => HttpResponse::NotFound().await.unwrap(),
     }
 }
 
 /// delete a tweet by its id `/tweets/{id}`
 #[delete("/tweets/{id}")]
-pub async fn delete(path: web::Path<(String,)>, pool: web::Data<DBPool>) -> impl Responder {
-    if let Ok(uuid) = Uuid::from_str(path.0 .0.as_str()) {
-        // use web::block to offload blocking Diesel code without blocking server thread
-        let result = web::block(move || {
-            let conn = pool.get().expect(CONNECTION_POOL_ERROR);
-            Tweet::delete(uuid, &conn)
-        })
-        .await;
+pub async fn delete(uid: web::Path<Uuid>, pool: web::Data<DBPool>) -> impl Responder {
+    let tweet_id = uid.into_inner();
 
-        match result {
-            Ok(_) => HttpResponse::Ok().await.unwrap(),
-            Err(_) => HttpResponse::NoContent().await.unwrap(),
-        }
-    } else {
-        HttpResponse::BadRequest().await.unwrap()
+    // use web::block to offload blocking Diesel code without blocking server thread
+    let result = web::block(move || {
+        let conn = pool.get().expect(CONNECTION_POOL_ERROR);
+        Tweet::delete(tweet_id, &conn)
+    })
+    .await;
+
+    match result {
+        Ok(_) => HttpResponse::Ok().await.unwrap(),
+        Err(_) => HttpResponse::BadRequest().await.unwrap(),
     }
 }
